277d6b6df2bd240db6f4d96850714708e8be7f16
debabratadutta <deb@analyzenbd.com>
2017-08-09 14:26:57 -0700

    NEW

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ function receivedPostbackMessage(event) {
                   break;
 
                 default:
-                  sendTextMessage(senderID, postbackPayloadg+" DD");
+                  sendTextMessage(senderID, " DD");
               }
             } 
       } 
